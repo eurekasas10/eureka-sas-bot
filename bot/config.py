61329f02cbf2d02e8328f18aa90bdc96275917f4
eurekasas10 <davimayermelhen@gmail.com>
2026-03-11 14:49:10 -0300

    Create bot configuration file with settings for interval time and API keys

---
+++ bot/config.py
@@ -0,0 +1,8 @@
+# Bot Configuration Settings
+
+# Interval time in seconds
+INTERVAL_TIME = 60  # Example: Update every 60 seconds
+
+# API Keys
+API_KEY_1 = 'your_first_api_key_here'
+API_KEY_2 = 'your_second_api_key_here'
